7c7e088c23d456397d20ac12a407eb5dfd36bec3
Esma <114703355+esmanurgokkaya@users.noreply.github.com>
2024-08-16 21:02:17 +0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ExcelReading.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
